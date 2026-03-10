3aabd41d1cf144bd1c28bd174e4efd3929ff11db
kkjw1 <kkjjoo1212@gmail.com>
2026-03-10 16:39:14 +0900

    예제 도메인 모델 추가

--- src/main/java/study/data_jpa/entity/Member.java
+++ src/main/java/study/data_jpa/entity/Member.java
@@ -1,25 +1,42 @@
 package study.data_jpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  // protected 타입의 생성자를 생성함
+@ToString(of = {"id", "username", "age"})       //객체를 바로 찍을 때 출력이 됨
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    // Entity는 기본적으로 디폴트 생성자가 하나 필요하다.
-    // JPA가 Proxy기술을 쓸 때 접근가능하도록 private로 만들면 안됨 protected까지는 열어놔야 됨
-    protected Member() {
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    //Entity는 기본적으로 디폴트 생성자가 하나 필요하다.
+    //JPA가 Proxy기술을 쓸 때 접근가능하도록 private로 만들면 안됨 protected까지는 열어놔야 됨
+//    protected Member() {}
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
